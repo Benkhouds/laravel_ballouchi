03774670c1a62d2de47e1bdc74a3a23e05423837
hamza <hamza.benkhoud@etudiant-isi.utm.tn>
2021-10-18 14:25:16 +0100

    cartitem seeder

--- database/seeders/CartItemSeeder.php
+++ database/seeders/CartItemSeeder.php
@@ -11,19 +11,28 @@ class CartItemSeeder extends Seeder
      * Run the database seeds.
      *
      * @return void
+     * @throws \Exception
      */
     public function run() : void
     {
+        $id=0 ;
+        while($id<10){
+            $arr = range(1,50);
+            $max =49 ;
+            $i=0;
+            while($i<7){
+                $x = random_int(0,$max);
+                CartItem::factory()->create([
+                    'cart_id'=>$id+1 ,
+                    'product_id'=>$arr[$x]
+                ]);
+                array_splice($arr, $x ,1);
+                $i++;
+                $max--;
+            }
+            $id++ ;
+        }
 
-        CartItem::factory()->count(7)->create();
-        CartItem::factory()->count(5)->create(['cart_id'=>2]);
-        CartItem::factory()->count(3)->create(['cart_id'=>3]);
-        CartItem::factory()->count(10)->create(['cart_id'=>4]);
-        CartItem::factory()->count(4)->create(['cart_id'=>5]);
-        CartItem::factory()->count(8)->create(['cart_id'=>6]);
-        CartItem::factory()->count(9)->create(['cart_id'=>7]);
-        CartItem::factory()->count(2)->create(['cart_id'=>8]);
-        CartItem::factory()->count(12)->create(['cart_id'=>9]);
-        CartItem::factory()->count(6)->create(['cart_id'=>10]);
+
     }
 }
